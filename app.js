2a503d1811cb04022d99101eb9e64f9e89082c6c
Bounty556 <jacobmayday@gmail.com>
2020-05-12 19:28:49 -0600

    Fixed Intern and Engineer questions parsing as numbers

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const engineerQuestions = [
         message: 'What is the Engineer\'s email?'
     },
     {
-        type: 'number',
+        type: 'input',
         name: 'github',
         message: 'What is the Engineer\'s github username?'
     }
@@ -74,7 +74,7 @@ const internQuestions = [
         message: 'What is the Intern\'s email?'
     },
     {
-        type: 'number',
+        type: 'input',
         name: 'school',
         message: 'What school does the Intern attend?'
     }
@@ -159,10 +159,7 @@ function askInternInfo() {
         });
 }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
+// Grabs template main html and inserts the generated html into it, finally writing it out to main.html
 function renderHTML() {
     const generatedHTML = render(employees);
     let mainTemplateHTML = fs.readFileSync(path.join(templatesDir, 'main.html'), 'utf-8');
@@ -177,19 +174,3 @@ function renderHTML() {
 
     console.log('Done');
 }
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
